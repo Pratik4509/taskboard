20f9dbf70284809bf6cb62e208a56f024ec86e7b
Pratik <pratikl2614@gmail.com>
2024-08-06 14:18:27 +0530

    Updated table and main section ui, added progress bar in projects

--- src/Components/Main.tsx
+++ src/Components/Main.tsx
@@ -8,22 +8,42 @@ const Main = () => {
                 <div className='text-xl font-semibold mb-2'>Home</div>
                 <p className='text-sm text-neutral-200'>Monitor all your projects from here</p>
             </div>
-            <div className='mx-auto mt-4 mb-8 w-full px-3 py-1.5 rounded border border-neutral-800 bg-light-black text-gray-300 flex justify-evenly items-center'>
-                <div className='w-full text-left border-r-2 border-dashed border-gray-600'>
-                    <div className='mb-2'>Total Projects</div>
-                    <p className='text-amber-500'>4</p>
+            <div className='mx-auto mt-4 mb-8 w-full px-3 py-1.5 text-gray-300 flex justify-between items-center gap-5'>
+                <div className='flex justify-between items-center w-full text-left bg-light-black border rounded border-neutral-800 p-4'>
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-7">
+                        <path strokeLinecap="round" strokeLinejoin="round" d="m21 7.5-2.25-1.313M21 7.5v2.25m0-2.25-2.25 1.313M3 7.5l2.25-1.313M3 7.5l2.25 1.313M3 7.5v2.25m9 3 2.25-1.313M12 12.75l-2.25-1.313M12 12.75V15m0 6.75 2.25-1.313M12 21.75V19.5m0 2.25-2.25-1.313m0-16.875L12 2.25l2.25 1.313M21 14.25v2.25l-2.25 1.313m-13.5 0L3 16.5v-2.25" />
+                    </svg>
+                    <div className=''>
+                        <div className='mb-2'>Total Projects</div>
+                        <p className='text-amber-500 text-right'>4</p>
+                    </div>
                 </div>
-                <div className='w-full text-left border-r-2 border-dashed border-gray-600 pl-8'>
-                    <div className='mb-2'>Total Tasks</div>
-                    <p className='text-pink-500'>4</p>
+                <div className='flex justify-between items-center w-full text-left bg-light-black border rounded border-neutral-800 p-4'>
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-7">
+                        <path strokeLinecap="round" strokeLinejoin="round" d="M6 6.878V6a2.25 2.25 0 0 1 2.25-2.25h7.5A2.25 2.25 0 0 1 18 6v.878m-12 0c.235-.083.487-.128.75-.128h10.5c.263 0 .515.045.75.128m-12 0A2.25 2.25 0 0 0 4.5 9v.878m13.5-3A2.25 2.25 0 0 1 19.5 9v.878m0 0a2.246 2.246 0 0 0-.75-.128H5.25c-.263 0-.515.045-.75.128m15 0A2.25 2.25 0 0 1 21 12v6a2.25 2.25 0 0 1-2.25 2.25H5.25A2.25 2.25 0 0 1 3 18v-6c0-.98.626-1.813 1.5-2.122" />
+                    </svg>
+                    <div>
+                        <div className='mb-2'>Total Tasks</div>
+                        <p className='text-pink-500 text-right'>4</p>
+                    </div>
                 </div>
-                <div className='w-full text-left border-r-2 border-dashed border-gray-600 pl-8'>
-                    <div className='mb-2'>Total Members</div>
-                    <p className='text-sky-500'>4</p>
+                <div className='flex justify-between items-center w-full text-left bg-light-black border rounded border-neutral-800 p-4'>
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-7">
+                        <path strokeLinecap="round" strokeLinejoin="round" d="M18 18.72a9.094 9.094 0 0 0 3.741-.479 3 3 0 0 0-4.682-2.72m.94 3.198.001.031c0 .225-.012.447-.037.666A11.944 11.944 0 0 1 12 21c-2.17 0-4.207-.576-5.963-1.584A6.062 6.062 0 0 1 6 18.719m12 0a5.971 5.971 0 0 0-.941-3.197m0 0A5.995 5.995 0 0 0 12 12.75a5.995 5.995 0 0 0-5.058 2.772m0 0a3 3 0 0 0-4.681 2.72 8.986 8.986 0 0 0 3.74.477m.94-3.197a5.971 5.971 0 0 0-.94 3.197M15 6.75a3 3 0 1 1-6 0 3 3 0 0 1 6 0Zm6 3a2.25 2.25 0 1 1-4.5 0 2.25 2.25 0 0 1 4.5 0Zm-13.5 0a2.25 2.25 0 1 1-4.5 0 2.25 2.25 0 0 1 4.5 0Z" />
+                    </svg>
+                    <div>
+                        <div className='mb-2'>Total Members</div>
+                        <p className='text-sky-500 text-right'>4</p>
+                    </div>
                 </div>
-                <div className='w-full text-left pl-8'>
-                    <div className='mb-2'>Tasks Completed</div>
-                    <p className='text-emerald-500'>4</p>
+                <div className='flex justify-between items-center w-full text-left bg-light-black border rounded border-neutral-800 p-4'>
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-7">
+                        <path strokeLinecap="round" strokeLinejoin="round" d="m21 7.5-9-5.25L3 7.5m18 0-9 5.25m9-5.25v9l-9 5.25M3 7.5l9 5.25M3 7.5v9l9 5.25m0-9v9" />
+                    </svg>
+                    <div>
+                        <div className='mb-2'>Tasks Completed</div>
+                        <p className='text-emerald-500 text-right'>4</p>
+                    </div>
                 </div>
             </div>
             <Table />
--- src/Components/Table.tsx
+++ src/Components/Table.tsx
@@ -8,29 +8,30 @@ import { FiArrowRight, FiPlus } from 'react-icons/fi'
 const Table = () => {
     const projects = useSelector((state: RootState) => state.kanban.projects)
     return (
-        <div className='project-table mt-4 border-t border-neutral-500 '>
+        <div className='project-table mt-4 border-t border-neutral-800 '>
             <div className='flex justify-between items-center'>
                 <h2 className="text-xl font-semibold my-6 text-left">Projects</h2>
-                <div className='px-3 py-1.5 bg-purple-700 rounded-md flex items-center justify-between cursor-pointer gap-1'>
+                <div className='px-3 py-1.5 bg-purple-700 hover:scale-110 transition-all rounded-md flex items-center justify-between cursor-pointer gap-1'>
                     <span>Add Task</span>
                     <FiPlus />
                 </div>
             </div>
-            <div className='px-3 py-1.5 rounded-lg border border-neutral-800 bg-light-black text-gray-300'>
+            <div className=' rounded-lg border border-neutral-800 bg-light-black text-gray-300'>
                 <table className='mx-auto w-full '>
-                    <thead className=''>
+                    {/* <thead className=''>
                         <tr >
-                            <th className='px-4 pt-2 pb-6 text-left '>Project Id</th>
                             <th className='px-4 pt-2 pb-6 text-left '>Project Name</th>
+                            <th className='px-4 pt-2 pb-6 text-left '>Project Description</th>
                             <th className='px-4 pt-2 pb-6 text-left '>Start Date</th>
                             <th className='px-4 pt-2 pb-6 text-left '>End Date</th>
                             <th className='px-4 pt-2 pb-6 text-left '>Actions</th>
                         </tr>
-                    </thead>
+                    </thead> */}
                     <tbody>
-                        {projects.map((project) => (
-                            <tr key={project.id} className="border-t border-gray-700 text-sm">
-                                <td className="px-4 py-4 text-gray-400 text-left ">
+                        {projects.map((project, index) => (
+                            <tr key={project.id}
+                                className={`border-b border-neutral-800 text-sm ${index === projects.length - 1 ? 'border-b-0' : ''}`}>
+                                <td className="px-4 py-4 text-gray-200 text-left ">
                                     <Link to={`/project/${project.id}`} className="hover:underline">
                                         {project.name}
                                     </Link>
